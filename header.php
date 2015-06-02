909c0e46d40ce95ed8dc74fa9f0ea8f9b8dc2788
Florencia Rosenfeld <hola@floristeady.com>
2015-06-02 11:43:39 -0500

    viewport

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
   <html <?php language_attributes(); ?> >
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 			
 	    <meta name="description" content="<?php echo '' . get_bloginfo ( 'description' );  ?>">
